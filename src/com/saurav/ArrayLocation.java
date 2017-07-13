f8c9e1ca8c014bf49bf41874c57ea704da0a723b
thesauravdas <sauravdas1584@gmail.com>
2017-07-13 15:14:47 -0700

    second java checkin

--- src/com/saurav/ArrayLocation.java
+++ src/com/saurav/ArrayLocation.java
@@ -14,7 +14,6 @@ public class ArrayLocation {
         double [] coords = {5.0,0.0};
         ArrayLocation accra= new ArrayLocation(coords);
         coords[0] = 32.9;
-        coords[1] = -117.2;
         System.out.println(accra.coords[0]);
     }
 }
