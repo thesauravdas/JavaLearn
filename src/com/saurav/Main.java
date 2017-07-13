670dae73933fa17ca08fa3cc1c5aee1cbf6d13f2
thesauravdas <sauravdas1584@gmail.com>
2017-07-13 15:07:07 -0700

    first java checkin

---
+++ src/com/saurav/Main.java
@@ -0,0 +1,19 @@
+package com.saurav;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x =5;
+        while (x > 0){
+            if (x == 1){
+                System.out.println("Hello World");
+            }
+            else{
+                System.out.println("Go to Hell");
+            }
+            x=x-1;
+        }
+
+	// write your code here
+    }
+}
